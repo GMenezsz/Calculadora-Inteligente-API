b5cd5b8a89c12ea6d6df2f2bf6133a580d0333cb
Gabriel Menezes <gabriel26guedes@gmail.com>
2026-08-25 17:42:54 -0300

    Update gastos.py

--- app/routers/gastos.py
+++ app/routers/gastos.py
@@ -1,15 +1,17 @@
 from fastapi import APIRouter, HTTPException
-from typing import  Optional
+from pydantic import BaseModel
 from calculadora import *
 
 router = APIRouter()
 
+class GastosRequest(BaseModel):
+    salario_liquido: float
+    gastos_essenciais: float
+
 @router.post("/calculadora_gastos")
-def api_gastos(salario_liquido: float, gastos_essenciais: float):
-    if validar_salario(salario_liquido) is not True or validar_gastos(gastos_essenciais) is not True:
+def api_gastos(dados: GastosRequest):
+    if validar_salario(dados.salario_liquido) is not True or validar_gastos(dados.gastos_essenciais) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
 
-    resultado = calcular_orcamento_50_30_20(salario_liquido, gastos_essenciais)
+    resultado = calcular_orcamento_50_30_20(dados.salario_liquido, dados.gastos_essenciais)
     return {"resultado": resultado}
-
-
