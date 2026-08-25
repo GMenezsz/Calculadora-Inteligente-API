43072d6669568ac03ec4d7f6710935b59f6c68b5
Gabriel Menezes <gabriel26guedes@gmail.com>
2026-08-25 17:41:50 -0300

    Update regra_tres.py

--- app/routers/regra_tres.py
+++ app/routers/regra_tres.py
@@ -1,12 +1,18 @@
 from calculadora import *
 from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
 
 router = APIRouter()
 
+class RegraTresRequest(BaseModel):
+    valor1: float
+    valor2: float
+    valor3: float
+
 @router.post("/calculadora_regra_tres")
-def api_calculadora_regra_tres(valor1: float, valor2: float, valor3: float):
-    if validar_regra_tres(valor1, valor2, valor3) is not True:
+def api_calculadora_regra_tres(dados: RegraTresRequest):
+    if validar_regra_tres(dados.valor1, dados.valor2, dados.valor3) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
     else:
-        resultado = calcular_regra_tres(valor1=valor1, valor2=valor2, valor3=valor3)
+        resultado = calcular_regra_tres(valor1=dados.valor1, valor2=dados.valor2, valor3=dados.valor3)
         return {"resultado": resultado}
