c24286c727233a341d143424b84c0d76fe1b54b6
Gabriel Menezes <gabriel26guedes@gmail.com>
2026-08-25 17:43:25 -0300

    Update eletrodomesticos.py

--- app/routers/eletrodomesticos.py
+++ app/routers/eletrodomesticos.py
@@ -1,18 +1,30 @@
 from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
 from calculadora import *
 
 router = APIRouter()
 
+class EletrodomesticosRequest(BaseModel):
+    potencia: int
+    horas_uso: float
+    dias_uso: int
+    valor_kwh: float
+
 @router.post("/calculadora_eletrodomesticos")
-def api_calculadora_eletrodomesticos(potencia: int, horas_uso: float, dias_uso: int, valor_kwh: float):
-    if validar_potencia(potencia) is not True:
+def api_calculadora_eletrodomesticos(dados: EletrodomesticosRequest):
+    if validar_potencia(dados.potencia) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
-    elif validar_horas_uso(horas_uso) is not True:
+    elif validar_horas_uso(dados.horas_uso) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
-    elif validar_dias_uso(dias_uso) is not True:
+    elif validar_dias_uso(dados.dias_uso) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
-    elif validar_valor_kwh(valor_kwh) is not True:
+    elif validar_valor_kwh(dados.valor_kwh) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
     else:
-        resultado = calcular_gastos(potencia=potencia, horas_uso=horas_uso, dias_uso=dias_uso, valor_kwh=valor_kwh)
+        resultado = calcular_gastos(
+            potencia=dados.potencia, 
+            horas_uso=dados.horas_uso, 
+            dias_uso=dados.dias_uso, 
+            valor_kwh=dados.valor_kwh
+        )
         return {"resultado": resultado}
