14beef4a24674f1bc10efab49a4e86061a96d70a
Gabriel Menezes <gabriel26guedes@gmail.com>
2026-08-25 18:51:47 -0300

    Update __init__.py

--- calculadora/__init__.py
+++ calculadora/__init__.py
@@ -1,3 +1,10 @@
 from .calculadora_gastos import * 
 from .calculadora_financiamento import *
 from .calculadora_media import *
+from . calculadora_juros_compostos import *
+from .calculadora_combustivel import *
+from .calculadora_motorista import *
+from .calculadora_eletrodomesticos import *
+from .calculadora_autonomos import *
+from .calculadora_regra_tres import *
+from .calculadora_alcool_gasolina import *
