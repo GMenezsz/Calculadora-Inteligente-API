e5d5363c4fc76ddab7fe30372d39754fe592199c
Gabriel Menezes <gabriel26guedes@gmail.com>
2026-08-25 17:15:34 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI
-from app.rotas import router as rotas
 from fastapi.middleware.cors import CORSMiddleware
+
+from app.routers.gastos import router as router_gastos
+from app.routers.media import router as router_media
+from app.routers.financiamento import router as router_financiamento
+from app.routers.juros_compostos import router as router_juros_compostos
+from app.routers.combustivel import router as router_combustivel
+from app.routers.motorista import router as router_motorista
+from app.routers.eletrodomesticos import router as router_eletrodomesticos
+from app.routers.autonomos import router as router_autonomos
+from app.routers.regra_tres import router as router_regra_tres
+from app.routers.alcool_gasolina import router as router_alcool_gasolina
 
 app = FastAPI(title="Calculadora Inteligente")
 
-# 1. Primeiro o CORS
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -13,8 +23,17 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# 2. Depois as rotas e o root
-app.include_router(rotas)
+app.include_router(router_gastos)
+app.include_router(router_media)
+app.include_router(router_financiamento)
+app.include_router(router_juros_compostos)
+app.include_router(router_combustivel)
+app.include_router(router_motorista)
+app.include_router(router_eletrodomesticos)
+app.include_router(router_autonomos)
+app.include_router(router_regra_tres)
+app.include_router(router_alcool_gasolina)
+
 
 @app.get("/")
 def read_root():
