f5a78ad6b1a72e61a11d686200ff633afd5fec32
Gabriel Menezes <gabriel26guedes@gmail.com>
2026-08-27 15:07:54 -0300

    Refactor validation checks in api_calculadora_motorista

--- app/routers/motorista.py
+++ app/routers/motorista.py
@@ -16,34 +16,41 @@ class MotoristaRequest(BaseModel):
 
 @router.post("/calculadora_motorista")
 def api_calculadora_motorista(dados: MotoristaRequest):
+    
     if validar_distancia(dados.distancia) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.") 
-    elif validar_consumo_veiculo(dados.consumo_veiculo) is not True:
+        
+    if validar_consumo_veiculo(dados.consumo_veiculo) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
-    elif validar_valor_combustivel(dados.valor_combustivel) is not True:
+        
+    if validar_valor_combustivel(dados.valor_combustivel) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
-    elif validar_ganhos(dados.ganhos) is not True:
+        
+    if validar_ganhos(dados.ganhos) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
-    elif validar_alimentacao(dados.alimentacao) is not True:
+        
+    if validar_alimentacao(dados.alimentacao) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
-    elif validar_cafe(dados.cafe) is not True:
+        
+    if validar_cafe(dados.cafe) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
-    elif validar_outros_gastos(dados.outros_gastos) is not True:
+        
+    if validar_outros_gastos(dados.outros_gastos) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
-    else:
-        resultado = calculadora_motorista(
-            distancia=dados.distancia, 
-            ganhos=dados.ganhos, 
-            consumo_veiculo=dados.consumo_veiculo, 
-            valor_combustivel=dados.valor_combustivel, 
-            alimentacao=dados.alimentacao, 
-            cafe=dados.cafe, 
-            outros_gastos=dados.outros_gastos
-        )
+    
+    resultado = calculadora_motorista(
+        distancia=dados.distancia, 
+        ganhos=dados.ganhos, 
+        consumo_veiculo=dados.consumo_veiculo, 
+        valor_combustivel=dados.valor_combustivel, 
+        alimentacao=dados.alimentacao, 
+        cafe=dados.cafe, 
+        outros_gastos=dados.outros_gastos
+    )
 
-        ganhos_km = resultado["ganhos_por_km"]
-        resultado_desempenho = resultado_quilometragem(ganhos_km)
-        return {
-            "resultado": resultado,
-            "desempenho": resultado_desempenho
-        }
+    ganhos_km = resultado["ganhos_por_km"]
+    resultado_desempenho = resultado_quilometragem(ganhos_km)
+    return {
+        "resultado": resultado,
+        "desempenho": resultado_desempenho
+    }
