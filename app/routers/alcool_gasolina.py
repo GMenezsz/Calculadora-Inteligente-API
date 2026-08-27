7d79c0f103d6812499ef84e95e836971429f90d5
Gabriel Menezes <gabriel26guedes@gmail.com>
2026-08-27 14:50:52 -0300

    Update alcool_gasolina.py

--- app/routers/alcool_gasolina.py
+++ app/routers/alcool_gasolina.py
@@ -12,11 +12,11 @@ class AlcoolGasolinaRequest(BaseModel):
 def api_alcool_gasolina(dados: AlcoolGasolinaRequest):
     if validar_alcool(dados.valor_alcool) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
-    elif validar_gasolina(dados.valor_gasolina) is not True:
+    if validar_gasolina(dados.valor_gasolina) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
-    else:
-        resultado = calcular_alcool_gasolina(
-            preco_alcool=dados.valor_alcool, 
-            preco_gasolina=dados.valor_gasolina
-        )
-        return {"resultado": resultado}
+    
+    resultado = calcular_alcool_gasolina(
+        preco_alcool=dados.valor_alcool, 
+        preco_gasolina=dados.valor_gasolina
+    )
+    return {"resultado": resultado}
