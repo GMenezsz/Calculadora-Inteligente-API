8a1bb5fee802bceb0b959360c3d1a89f3481f3b0
Gabriel Menezes <gabriel26guedes@gmail.com>
2026-08-27 14:52:48 -0300

    Update eletrodomesticos.py

--- app/routers/eletrodomesticos.py
+++ app/routers/eletrodomesticos.py
@@ -14,17 +14,17 @@ class EletrodomesticosRequest(BaseModel):
 def api_calculadora_eletrodomesticos(dados: EletrodomesticosRequest):
     if validar_potencia(dados.potencia) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
-    elif validar_horas_uso(dados.horas_uso) is not True:
+    if validar_horas_uso(dados.horas_uso) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
-    elif validar_dias_uso(dados.dias_uso) is not True:
+    if validar_dias_uso(dados.dias_uso) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
-    elif validar_valor_kwh(dados.valor_kwh) is not True:
+    if validar_valor_kwh(dados.valor_kwh) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
-    else:
-        resultado = calcular_gastos(
-            potencia=dados.potencia, 
-            horas_uso=dados.horas_uso, 
-            dias_uso=dados.dias_uso, 
-            valor_kwh=dados.valor_kwh
-        )
-        return {"resultado": resultado}
+    
+    resultado = calcular_gastos(
+        potencia=dados.potencia, 
+        horas_uso=dados.horas_uso, 
+        dias_uso=dados.dias_uso, 
+        valor_kwh=dados.valor_kwh
+    )
+    return {"resultado": resultado}
