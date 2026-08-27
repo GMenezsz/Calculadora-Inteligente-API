5eb0da52799e0fc317a7a8fd246d4b26592909f7
Gabriel Menezes <gabriel26guedes@gmail.com>
2026-08-27 15:03:28 -0300

    Add validation for usage parameters in API

--- app/routers/eletrodomesticos.py
+++ app/routers/eletrodomesticos.py
@@ -14,13 +14,16 @@ class EletrodomesticosRequest(BaseModel):
 def api_calculadora_eletrodomesticos(dados: EletrodomesticosRequest):
     if validar_potencia(dados.potencia) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
+        
     if validar_horas_uso(dados.horas_uso) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
+        
     if validar_dias_uso(dados.dias_uso) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
+        
     if validar_valor_kwh(dados.valor_kwh) is not True:
         raise HTTPException(status_code=400, detail="Valores inválidos.")
-    
+        
     resultado = calcular_gastos(
         potencia=dados.potencia, 
         horas_uso=dados.horas_uso, 
