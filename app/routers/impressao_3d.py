820f760348695b64838959221aa6e89891372530
Gabriel Menezes <gabriel26guedes@gmail.com>
2026-09-04 17:23:34 -0300

    Update impressao_3d.py

--- app/routers/impressao_3d.py
+++ app/routers/impressao_3d.py
@@ -34,28 +34,31 @@ def api_calcular_impressao3d(dados: ImpressaoRequest):
         if validar_custo_maquina(dados.custo_maquina) is not True:
                 raise HTTPException(status_code=400, detail="O custo da maquina não pode ser menor que 0.")
 
-        if validar_margem_lucro(dados.margem_lucro_percentual) is not True:
+        if validar_margem_lucro_impressao(dados.margem_lucro_percentual) is not True:
                 raise HTTPException(status_code=400, detail="A margem de lucro não pode ser menor que 0 e nem maior ou igual a 100.")
 
         if validar_insumos(dados.insumos) is not True:
                 raise HTTPException(status_code=400, detail="O valor dos insumos não pode ser menor que 0.")
 
-        if validar_taxa_maquininha(dados.taxa_maquininha) is not True:
+        if validar_taxa_maquininha_impressao(dados.taxa_maquininha) is not True:
                 raise HTTPException(status_code=400, detail="A taxa da maquininha não pode ser menor que 0.")
 
-        if validar_deslocamento(dados.deslocamento_entrega) is not True:
+        if validar_deslocamento_impressao(dados.deslocamento_entrega) is not True:
                 raise HTTPException(status_code=400, detail="O valor de deslocamento/entrega não pode ser menor que 0.")
 
-        resultado = calcular_custos_impressao(
-                filamento_g=dados.filamento_g,
-                preco_filamento_kg=dados.preco_filamento_kg,
-                tempo_impressao=dados.tempo_impressao,
-                custo_energia=dados.custo_energia,
-                custo_maquina=dados.custo_maquina,
-                margem_lucro_percentual=dados.margem_lucro_percentual,
-                insumos=dados.insumos,
-                taxa_maquininha=dados.taxa_maquininha,
-                deslocamento_entrega=dados.deslocamento_entrega
-        )
+        try:
+                resultado = calcular_custos_impressao(
+                        filamento_g=dados.filamento_g,
+                        preco_filamento_kg=dados.preco_filamento_kg,
+                        tempo_impressao=dados.tempo_impressao,
+                        custo_energia=dados.custo_energia,
+                        custo_maquina=dados.custo_maquina,
+                        margem_lucro_percentual=dados.margem_lucro_percentual,
+                        insumos=dados.insumos,
+                        taxa_maquininha=dados.taxa_maquininha,
+                        deslocamento_entrega=dados.deslocamento_entrega
+                )
+        except ValueError as erro:
+                raise HTTPException(status_code=400, detail=str(erro))
 
         return {"resultado": resultado}
